7311aabe9faed3bf52bcddaff76dbb15e8789f9c
Javlon <abdurasulov2203@gmail.com>
2023-12-08 17:06:15 +0500

    s

--- ServiceCatalog.Domain/Entity/Stadium/Field.cs
+++ ServiceCatalog.Domain/Entity/Stadium/Field.cs
@@ -6,23 +6,8 @@ using System.Threading.Tasks;
 
 namespace ServiceCatalog.Domain.Entity.Stadium
 {
-	public class Field : ICategory
+	public class Field 
 	{
-		//public int CategoryId { get; set; } = 1;
-		//public int Id { get; set; }
-		//      public string Name { get; set; }
-		//public string Address { get; set; }
-		//public string Size { get; set; }
-		//public decimal Price { get; set; }
-		//public string Description { get; set; }
-		//public byte[] ImageData { get; set; }
-
-		//// TimeSpan.FromHours(10) + TimeSpan.FromMinutes(30);
-		//public TimeSpan OpeningTime { get; set; }
-		//public TimeSpan ClosingTime { get; set; }
-
-		////bronda bu boladi
-		////public List<bool> IsAvailable { get; set; }
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public int Location { get; set; }
