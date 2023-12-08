999df86de1a9f34d37635a35c64b337cfee6bdd9
Asror <asrorrustmaovich007@gmail.com>
2023-12-08 16:24:23 +0500

    Qoshdim

---
+++ Identity.Domain/Models/UserRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Models
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
---
+++ Identity.Infrastructure/Dbcontext/ApplicationDbcontext.cs
@@ -0,0 +1,23 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Dbcontext
+{
+    public class ApplicationDbcontext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbcontext()
+        {
+
+        }
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options)
+        {
+
+        }
+    }
+}
