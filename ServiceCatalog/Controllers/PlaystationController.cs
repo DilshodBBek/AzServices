21de012e2cfb50947a1f8240262622038fe594b4
Shokir <tursinbekovshokirxon@gmail.com>
2023-12-08 15:30:42 +0500

    фыв

--- ServiceCatalog/Controllers/PlaystationController.cs
+++ ServiceCatalog/Controllers/PlaystationController.cs
@@ -1,6 +1,19 @@
-﻿namespace ServiceCatalog.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceCatalog.Domain.DTO.Playstation;
+using System.ComponentModel;
+
+namespace ServiceCatalog.Controllers
 {
-    public class PlaystationController
+    [Route("api/[controller]/[action]")]
+    public class PlaystationAreaController:Controller
     {
+        public PlaystationAreaController()
+        {
+            
+        }
+        public string CreatePlaystationArea(PlaystationCreateDTO obj)
+        {
+
+        }
     }
 }
