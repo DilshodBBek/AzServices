c6d60fcfc42a086224a316603af0030468c422d6
Javlon <abdurasulov2203@gmail.com>
2023-12-09 16:54:32 +0500

    ы

---
+++ ServiceCatalog.Application/Inrefaces/Booking/ICRUDRepositoryBooking.cs
@@ -0,0 +1,20 @@
+﻿using ServiceCatalog.Application.Inrefaces.Base;
+using ServiceCatalog.Domain.Entity;
+using ServiceCatalog.Domain.Entity.Playstation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCatalog.Application.Inrefaces.Booking
+{
+	public interface ICRUDRepositoryBooking:
+		ICreateService<ServiceCatalog.Domain.Entity.Booking>,
+		IUpdateService<ServiceCatalog.Domain.Entity.Booking>,
+		IGetByIdService<ServiceCatalog.Domain.Entity.Booking>,
+		IDeleteService,
+		IGetAllService<ServiceCatalog.Domain.Entity.Booking>
+	{
+	}
+}
---
+++ ServiceCatalog.Domain/DTO/Booking/BookingCreateDTO.cs
@@ -0,0 +1,20 @@
+﻿using ServiceCatalog.Domain.Entity.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCatalog.Domain.DTO.Booking
+{
+	public class BookingCreateDTO
+	{
+		public DateTime BroneTime { get; set; }
+		public TimeSpan Duration { get; set; }
+		public string BronePhoneNumber { get; set; }
+
+		// Связь с категорией поля через общий интерфейс
+		public int ServiceId { get; set; }
+		public Base Service { get; set; }
+	}
+}
