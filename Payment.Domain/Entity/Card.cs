c09ba1da06689c4a53aa9eaf6e1d06b02ac3fdb6
Asror <asrorrustmaovich007@gmail.com>
2023-12-09 21:47:09 +0500

    asdas

--- Payment.Domain/Entity/Card.cs
+++ Payment.Domain/Entity/Card.cs
@@ -11,5 +11,4 @@ public class Card
         [RegularExpression(@"^(0[1-9]|1[0-2])\/\d{2}$", ErrorMessage = "Not a valid expiration date.")]
        
         public string ExpirationDate { get; set; } 
-    }
 }
