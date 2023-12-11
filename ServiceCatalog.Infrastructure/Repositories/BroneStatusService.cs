5b1d160da19eace4a7477550eb43b44f4ab20826
Javlon <abdurasulov2203@gmail.com>
2023-12-11 18:51:39 +0500

    s

--- ServiceCatalog.Infrastructure/Repositories/BroneStatusService.cs
+++ ServiceCatalog.Infrastructure/Repositories/BroneStatusService.cs
@@ -1,5 +1,7 @@
-﻿using ServiceCatalog.Application.Inrefaces;
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceCatalog.Application.Inrefaces;
 using ServiceCatalog.Application.Inrefaces.Playstation;
+using ServiceCatalog.Domain.Entity.Common;
 using ServiceCatalog.Domain.Entity.Playstation;
 using ServiceCatalog.Infrastructure.Data.Contex;
 using System;
@@ -17,10 +19,35 @@ namespace ServiceCatalog.Infrastructure.Repositories
 		{
 			_db = db;
 		}
-		public /*async*/ List<Cabin> GetStatus(int CabinaId)
+		public async List<Cabin> GetStatus(int CabinaId)
 		{
-			//var cabina = _db.Ca
-			return null;
+			var cabina = _db.Cabins.Include(x=>x.BroneStatuses).FirstOrDefault(x => x.Id == CabinaId);
+			List<Domain.Entity.Common.BroneStatus> status = cabina.BroneStatuses;
+			DateTime currentDate = DateTime.Today;
+
+			var allTimeRanges = Enumerable.Range(0, 24)
+			.Select(hour => currentDate.AddHours(hour))
+			.ToList();
+
+			// Фильтруем и сортируем статусы
+			var filteredAndSortedStatus = status
+				.Where(item => item.TimeDiapazonId >= currentDate && item.TimeDiapazonId < currentDate.AddDays(1))
+				.OrderBy(item => item.TimeDiapazonId)
+				.ToList();
+
+			var mergedList = allTimeRanges
+			.GroupJoin(filteredAndSortedStatus,
+					   range => range,
+					   statusItem => statusItem.TimeDiapazonId,
+					   (range, statuses) => statuses.DefaultIfEmpty(new BroneStatus { TimeDiapazonId = range, BookingStatusId = 3 }))
+			.SelectMany(group => group)
+			.ToList();
+
+			//foreach (var item in filteredAndSortedStatus)
+			//{
+			//	//DateTime
+			//	item.TimeDiapazonId
+			//}
 		}
 	}
 }
