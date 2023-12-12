526e901d43558bbb37281f49da4cdc4f1458f93f
Muhammadali <qodirovmuhammadali246@gmail.com>
2023-12-12 15:28:58 +0500

    bot

--- Notification.Infrastructure/Service/TelegramBotDbServise.cs
+++ Notification.Infrastructure/Service/TelegramBotDbServise.cs
@@ -1,7 +1,17 @@
 ﻿using Notification.Application.Interfaces;
+using Notification.Domain.Entities;
 
 namespace Notification.Infrastructure.Service;
 
-public class TelegramBotDbServise:ITelegramBotServise
+public class TelegramBotDbServise : ITelegramBotServise
 {
+    public Task<TelegramBotEntity> GetUserAsync(TelegramBotEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SendMassageAsync(TelegramBotEntity entity)
+    {
+        throw new NotImplementedException();
+    }
 }
