c9be5d618ebb2b96db9bdc6cfcf6d312ff85f3cd
sardorbek002 <sardorbekibroximov0602@gmail.com>
2023-12-14 21:37:44 +0500

    permission

--- Identity.Domain/Entities/Role.cs
+++ Identity.Domain/Entities/Role.cs
@@ -1,17 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace Identity.Domain.Entities;
 
-namespace Identity.Domain.Entities
+public class Role : IdentityRole<int>
 {
-    public class Role:IdentityRole<int>
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        //public virtual ICollection<ApplicationUser>? Users { get; set; }
-        public virtual ICollection<permission>? Permissions { get; set; }
-    }
+    public virtual ICollection<permission>? Permissions { get; set; }
 }
