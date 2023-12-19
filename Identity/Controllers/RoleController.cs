336d6e89c819b49284a08b2194dcb401f154d176
Asror <asrorrustmaovich007@gmail.com>
2023-12-19 18:55:46 +0500

    ozgardi

--- Identity/Controllers/RoleController.cs
+++ Identity/Controllers/RoleController.cs
@@ -25,7 +25,15 @@ namespace Identity.Controllers
         [HttpGet("GetAllRoles")]
         public IActionResult GetAllRoles()
         {
-            var roles = _dbcontext.Roles.Include(x=>x.Permissions).ToList();
+            List<RoleGetDTO> roles = _dbcontext.Roles
+    .Include(x => x.Permissions)
+    .Select(x => new RoleGetDTO
+    {
+       Permissionids=x.Permissions.Select(x=>x.id).ToList(),
+       Id=x.Id,
+       Name=x.Name
+    }).ToList();
+
             return Ok(roles);
         }
 
