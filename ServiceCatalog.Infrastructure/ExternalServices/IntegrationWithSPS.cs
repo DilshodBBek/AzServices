334a36bfae4b24a1f5c7b9e08ad854dcb5ad9062
Shokir <tursinbekovshokirxon@gmail.com>
2023-12-24 10:47:50 +0500

    asd

---
+++ ServiceCatalog.Infrastructure/ExternalServices/IntegrationWithSPS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCatalog.Infrastructure.ExternalServices
+{
+    public class IntegrationWithSPS
+    {
+    }
+}
